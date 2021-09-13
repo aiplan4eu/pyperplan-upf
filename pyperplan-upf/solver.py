c9bf33fdb798c675bd581ea2340742d67cbcf9c6
Luca Framba <framba@fbk.eu>
2021-09-13 08:53:28 +0200

    Print changes for debug

--- pyperplan-upf/solver.py
+++ pyperplan-upf/solver.py
@@ -74,8 +74,14 @@ class SolverImpl(upf.Solver):
         visitor = TraversePDDLProblem(dom)
         # and traverse the AST
         probAST.accept(visitor)
-        # finally return the pddl.Problem
-        task = _ground(visitor.get_problem())
+        prob = visitor.get_problem()
+        print("PROBELM PARSED:")
+        print(prob)
+        print(dom)
+        #< Problem definition: robot Domain: domain_robot Objects: ['location', 'location'] Initial State: ["robot_at[('l1', location)]"] Goal State : ["robot_at[('l2', (location,))]"] >
+        #< Problem definition: robot Domain: domain_robot Objects: ['location', 'location'] Initial State: ["robot_at[('l1', location)]"] Goal State : ["robot_at[('l2', (location,))]"] >
+        #  finally return the pddl.Problem
+        task = _ground(prob)
         heuristic = None
         # if not heuristic_class is None:
         #     heuristic = heuristic_class(task)
